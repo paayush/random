e57da46b72948a9356d173922bb0bc20adf389cf
Aayush pokharel <pokharelaayush199@gmail.com>
2022-11-17 15:30:05 +0545

    random array

---
+++ beginners.py
@@ -0,0 +1,17 @@
+import random
+
+arr = []
+div2 = []
+div = []
+
+for i in range(1,20):
+    num = random.randint(1,20)
+    arr.append(num)
+    if (num % 2 == 0) and (num != 0):
+        div2.append(num)
+    else:
+        div.append(num)
+
+print(f"number in an array {arr}")
+print(f"\nnumber in an array which are divisible by 2 {div2}")
+print(f"\nnumber in an array which are not divisible by 2 {div}")
